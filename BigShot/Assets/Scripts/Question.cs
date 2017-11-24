c045953c71b9d16e7c8625435d20b35a6095729d
HIGHPOWEREDTOAS\Pedro <pedrodossantosmarques@gmail.com>
2017-11-24 13:59:15 +0100

    Start HUD, reorganized scene, 🅱️

---
+++ BigShot/Assets/Scripts/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question {
+
+    public string QuestionText { get; set; }
+    public List<string> Answers { get; set; }
+
+    public Question(List<string> answers, string question)
+    {
+        Answers = answers;
+        QuestionText = question;
+    }
+
+
+}
