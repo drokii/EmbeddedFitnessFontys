4d3e4961dc6f9e74b4eff5401565e21170644f71
Daan Janssen <I365864@student.fontys.nl>
2017-11-27 14:10:30 +0100

    Bubbles bouncen van elkaar af

--- BigShot/Assets/Scripts/BubbleBounce.cs
+++ BigShot/Assets/Scripts/BubbleBounce.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class BubbleBounce : MonoBehaviour {
     int edgeX, edgeY;
+    public int borderMargin = 50;
     bool movingRight, movingDown, movingUp, movingLeft;
+    public float speed = .5f;
 
 	// Use this for initialization
 	void Start () {
-        edgeX = Screen.width;
-        edgeY = Screen.height;
+        edgeX = Screen.width - borderMargin;
+        edgeY = Screen.height - borderMargin;
         switch(Random.Range(1, 3))
         {
             case 1:
@@ -42,44 +44,44 @@ public class BubbleBounce : MonoBehaviour {
 	void Update () {
         if (transform.position.x < edgeX && movingRight)
         {
-            transform.Translate(1, 0, 0);
+            transform.Translate(speed, 0, 0);
         }
         else if (transform.position.x > 0)
         {
-            transform.Translate(-1, 0, 0);
+            transform.Translate(-speed, 0, 0);
             movingRight = false;
             movingLeft = true;
         }
 
-        if (transform.position.x > 0 && movingLeft)
+        if (transform.position.x > borderMargin && movingLeft)
         {
-            transform.Translate(-1, 0, 0);
+            transform.Translate(-speed, 0, 0);
         }
         else if (transform.position.x < edgeX)
         {
-            transform.Translate(1, 0, 0);
+            transform.Translate(speed, 0, 0);
             movingRight = true;
             movingLeft = false;
         }
 
         if (transform.position.y < edgeY && movingDown)
         {
-            transform.Translate(0, 1, 0);
+            transform.Translate(0, speed, 0);
         }
         else if (transform.position.y > 0)
         {
-            transform.Translate(0, -1, 0);
+            transform.Translate(0, -speed, 0);
             movingDown = false;
             movingUp = true;
         }
 
-        if (transform.position.y > 0 && movingUp)
+        if (transform.position.y > borderMargin && movingUp)
         {
-            transform.Translate(0, -1, 0);
+            transform.Translate(0, -speed, 0);
         }
         else if (transform.position.y < edgeY)
         {
-            transform.Translate(0, 1, 0);
+            transform.Translate(0, speed, 0);
             movingDown = true;
             movingUp = false;
         }
@@ -87,8 +89,6 @@ public class BubbleBounce : MonoBehaviour {
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Hit");
-        Debug.Log(transform.position.x);
         if (collision.transform.position.x > transform.position.x)
         {
             movingLeft = true; movingRight = false;
